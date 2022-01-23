5af5361a29feace8896425c7952bf0b588597eb2
Nishant Sapkota <snishant306@gmail.com>
2022-01-23 18:40:03 +0545

    feat: add routes for Views

--- src/views.py
+++ src/views.py
@@ -1,3 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
+
+
+@views.get("/")
+def homepage():
+    return render_template("index.html")
