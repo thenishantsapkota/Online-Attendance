119cb364ace615f0dd88b0d7602379abc070fbd5
Nishant Sapkota <snishant306@gmail.com>
2022-01-23 18:39:46 +0545

    feat: add routes for Authentication

--- src/auth.py
+++ src/auth.py
@@ -1,3 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, render_template, url_for
 
 auth = Blueprint("auth", __name__)
+
+
+@auth.get("/login")
+def login() -> str:
+    return render_template("login.html")
+
+
+@auth.get("/signup")
+def signup() -> str:
+    return render_template("signup.html")
+
+
+@auth.get("/logout")
+def logout():
+    return redirect(url_for("views.homepage"))
