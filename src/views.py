1db3e30ed9a9f6d3aa90e6fa5a03e3fe618fb135
Nishant Sapkota <snishant306@gmail.com>
2022-01-24 18:50:58 +0545

    refactor: add profile route and utilized base.html in profile.html

--- src/views.py
+++ src/views.py
@@ -5,4 +5,9 @@ views = Blueprint("views", __name__)
 
 @views.get("/")
 def homepage():
-    return render_template("base.html")
+    return render_template("index.html")
+
+
+@views.get("/profile")
+def profile():
+    return render_template("profile.html")
