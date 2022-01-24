eb32f8254ab4dae69438642c9e9dd701f5d38490
Nishant Sapkota <snishant306@gmail.com>
2022-01-24 20:58:46 +0545

    refactor: pass user=current_user on every render_template

--- src/views.py
+++ src/views.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 views = Blueprint("views", __name__)
 
 
 @views.get("/")
 def homepage():
-    return render_template("index.html")
+    return render_template("index.html", user=current_user)
 
 
 @views.get("/profile")
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", user=current_user)
